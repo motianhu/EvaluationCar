1bc7efae75bfc2802a70ead4ec982726f5275154
moth <moth@gionee.com>
2016-12-15 22:37:17 +0800

    init project

---
+++ app/src/main/java/com/smona/app/evalution/evaluationcar/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.smona.app.evalution.evaluationcar;
+
+import android.app.Activity;
+
+/**
+ * Created by Moth on 2016/12/15.
+ */
+
+public abstract class BaseActivity extends Activity {
+}
---
+++ app/src/main/java/com/smona/app/evalution/evaluationcar/HomeActivity.java
@@ -0,0 +1,8 @@
+package com.smona.app.evalution.evaluationcar;
+
+/**
+ * Created by Moth on 2016/12/15.
+ */
+
+public class HomeActivity extends BaseActivity {
+}
