f49fa1a7262614d864661d6621df9eb7175d86ee
moth <moth@gionee.com>
2017-03-23 18:14:31 +0800

    相机UI调整

--- app/src/main/java/com/smona/app/evaluationcar/util/UrlConstants.java
+++ app/src/main/java/com/smona/app/evaluationcar/util/UrlConstants.java
@@ -17,12 +17,15 @@ public class UrlConstants {
 
     private static Map<Integer, String> INTEFACES = new HashMap<>();
 
-    public static final int CREATE_CARBILLID = 1;
-    public static final int UPLOAD_IMAGE = 2;
-    public static final int SUBMIT_CARBILL = 3;
-    public static final int QUERY_IMAGEMETA = 4;
+
+    public static final int CHECK_USER = 1;
+    public static final int CREATE_CARBILLID = 2;
+    public static final int UPLOAD_IMAGE = 3;
+    public static final int SUBMIT_CARBILL = 4;
+    public static final int QUERY_IMAGEMETA = 5;
 
     static {
+        INTEFACES.put(CHECK_USER, "external/checkUser.html");
         INTEFACES.put(CREATE_CARBILLID, "external/carBill/getCarBillIdNextVal.html");
         INTEFACES.put(UPLOAD_IMAGE, "external/app/uploadAppImage.html");
         INTEFACES.put(SUBMIT_CARBILL, "external/app/finishCreateAppCarBill.html");
