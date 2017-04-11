a6ce848494d3b46fd14d4d70be894b966f1420ed
moth <moth@gionee.com>
2017-04-11 20:15:09 +0800

    搜索

--- app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/SearchActivity.java
+++ app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/SearchActivity.java
@@ -2,18 +2,35 @@ package com.smona.app.evaluationcar.ui.evaluation;
 
 import android.os.Bundle;
 
+import com.smona.app.evaluationcar.R;
 import com.smona.app.evaluationcar.ui.common.activity.BaseActivity;
+import com.smona.app.evaluationcar.ui.common.activity.HeaderActivity;
 
 
 /**
  * Created by Moth on 2017/3/13.
  */
 
-public class SearchActivity extends BaseActivity {
+public class SearchActivity extends HeaderActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search;
+    }
+
+    @Override
+    protected boolean showDelete() {
+        return false;
+    }
+
+    @Override
+    protected int getHeaderTitle() {
+        return R.string.search_title;
     }
 
 
