f483e36cc10910bd01999566bbf07a0d6b10ad68
moth <moth@gionee.com>
2017-04-17 22:53:59 +0800

    提交上牌时间

--- app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/PreEvaluationEditLayer.java
+++ app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/PreEvaluationEditLayer.java
@@ -1,13 +1,18 @@
 package com.smona.app.evaluationcar.ui.evaluation.preevaluation;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.smona.app.evaluationcar.R;
 import com.smona.app.evaluationcar.util.ActivityUtils;
+import com.smona.app.evaluationcar.util.ViewUtil;
+
+import java.util.Calendar;
 
 /**
  * Created by motianhu on 4/15/17.
@@ -16,6 +21,7 @@ import com.smona.app.evaluationcar.util.ActivityUtils;
 public class PreEvaluationEditLayer extends RelativeLayout {
 
     private TextView mCarModel;
+    private TextView mCarDate;
 
     public PreEvaluationEditLayer(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,6 +31,8 @@ public class PreEvaluationEditLayer extends RelativeLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         mCarModel = (TextView) findViewById(R.id.tv_type_content);
+        mCarDate = (TextView) findViewById(R.id.tv_timecontent);
+
         findViewById(R.id.container_cartype).setOnClickListener(mOnClickListener);
         findViewById(R.id.container_cartime).setOnClickListener(mOnClickListener);
         findViewById(R.id.container_city).setOnClickListener(mOnClickListener);
@@ -37,7 +45,8 @@ public class PreEvaluationEditLayer extends RelativeLayout {
             int id = v.getId();
             switch (id) {
                 case R.id.container_cartime:
-                    ActivityUtils.jumpOnlyActivity(getContext(), TimePickerActivity.class);
+                    //ActivityUtils.jumpOnlyActivity(getContext(), TimePickerActivity.class);
+                    initSelectDate();
                     break;
                 case R.id.container_cartype:
                     ActivityUtils.jumpOnlyActivity(getContext(), CarTypeActivity.class);
@@ -49,4 +58,44 @@ public class PreEvaluationEditLayer extends RelativeLayout {
 
         }
     };
+
+    private void initSelectDate() {
+        View view = ViewUtil.inflater(getContext(), R.layout.activity_timepicker);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+        initNumberPicker(dialog, view);
+        dialog.show();
+    }
+
+    private void initNumberPicker(final AlertDialog dialog, View view) {
+        final Calendar calendar = Calendar.getInstance();
+        int mYear = calendar.get(Calendar.YEAR);
+
+        final NumberPicker year = (NumberPicker) view.findViewById(R.id.selecYear);
+        year.setMaxValue(mYear);
+        year.setMinValue(mYear - 6);
+        year.setFocusable(false);
+        year.setFocusableInTouchMode(false);
+        year.setWrapSelectorWheel(true);
+        year.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        final NumberPicker month = (NumberPicker) view.findViewById(R.id.selectMonth);
+        month.setMaxValue(12);
+        month.setMinValue(1);
+        month.setFocusable(false);
+        month.setFocusableInTouchMode(false);
+        month.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        view.findViewById(R.id.finish_select).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String time = String.format(getContext().getResources().getString(R.string.car_time),
+                        year.getValue() + "", month.getValue() + "");
+                mCarDate.setText(time);
+                dialog.dismiss();
+            }
+        });
+    }
 }
--- app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/TimePickerActivity.java
+++ app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/TimePickerActivity.java
@@ -1,9 +1,10 @@
 package com.smona.app.evaluationcar.ui.evaluation.preevaluation;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+
 import com.smona.app.evaluationcar.R;
 import com.smona.app.evaluationcar.ui.common.activity.BaseActivity;
-import com.smona.app.evaluationcar.ui.common.numberpicker.NumberPicker;
 
 import java.util.Calendar;
 
@@ -16,22 +17,26 @@ public class TimePickerActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timepicker);
-        final Calendar c = Calendar.getInstance();
-        int mYear = c.get(Calendar.YEAR);
-        int mMonth = c.get(Calendar.MONTH);
+        final Calendar calendar = Calendar.getInstance();
+        int mYear = calendar.get(Calendar.YEAR);
+        int mMonth = calendar.get(Calendar.MONTH);
 
         NumberPicker year = (NumberPicker) findViewById(R.id.selecYear);
-        year.setMaxValue(10);
+        year.setMaxValue(mYear);
         year.setMinValue(mYear - 10);
         year.setFocusable(false);
         year.setFocusableInTouchMode(false);
-
+        year.setWrapSelectorWheel(true);
+        year.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         NumberPicker month = (NumberPicker) findViewById(R.id.selectMonth);
         month.setMaxValue(12);
         month.setMinValue(1);
         month.setFocusable(false);
         month.setFocusableInTouchMode(false);
+        month.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+
     }
 
     @Override
