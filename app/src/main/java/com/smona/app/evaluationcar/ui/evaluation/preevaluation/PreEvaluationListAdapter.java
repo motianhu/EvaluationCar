c795fe15206021ed20ac03b0f13fea77953ed84f
moth <moth@gionee.com>
2017-04-21 17:22:41 +0800

    预安装列表

--- app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/PreEvaluationListAdapter.java
+++ app/src/main/java/com/smona/app/evaluationcar/ui/evaluation/preevaluation/PreEvaluationListAdapter.java
@@ -1,20 +1,14 @@
 package com.smona.app.evaluationcar.ui.evaluation.preevaluation;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.smona.app.evaluationcar.R;
 import com.smona.app.evaluationcar.data.bean.PreCarBillBean;
-import com.smona.app.evaluationcar.framework.imageloader.ImageLoaderProxy;
-import com.smona.app.evaluationcar.ui.evaluation.EvaluationActivity;
-import com.smona.app.evaluationcar.util.ActivityUtils;
-import com.smona.app.evaluationcar.util.StatusUtils;
 import com.smona.app.evaluationcar.util.ViewUtil;
 
 import java.util.ArrayList;
@@ -63,14 +57,29 @@ public class PreEvaluationListAdapter extends BaseAdapter implements View.OnClic
         PreCarBillBean carbill = mDataList.get(position);
         if (convertView == null) {
             convertView = ViewUtil.inflater(mContext,
-                    R.layout.status_list_local_item);
+                    R.layout.status_list_preevalution_item);
         }
 
         convertView.setOnClickListener(this);
         convertView.setTag(carbill);
 
-        TextView textTime = (TextView) convertView.findViewById(R.id.carTime);
-        textTime.setText(mContext.getString(R.string.list_item_time) + " " + carbill.createTime);
+        TextView carType = (TextView) convertView.findViewById(R.id.carType);
+        carType.setText(mContext.getString(R.string.list_item_time) + " " + carbill.createTime);
+
+        TextView carColor = (TextView) convertView.findViewById(R.id.carColor);
+        carColor.setText(mContext.getString(R.string.list_item_time) + " " + carbill.color);
+
+        TextView carNum = (TextView) convertView.findViewById(R.id.runNum);
+        carNum.setText(mContext.getString(R.string.list_item_time) + " " + carbill.runNum);
+
+        TextView textTime = (TextView) convertView.findViewById(R.id.regDate);
+        textTime.setText(mContext.getString(R.string.list_item_time) + " " + carbill.regDate);
+
+        TextView city = (TextView) convertView.findViewById(R.id.city);
+        city.setText(mContext.getString(R.string.list_item_time) + " " + carbill.city);
+
+        TextView mark = (TextView) convertView.findViewById(R.id.mark);
+        mark.setText(mContext.getString(R.string.list_item_time) + " " + carbill.mark);
 
         return convertView;
     }
