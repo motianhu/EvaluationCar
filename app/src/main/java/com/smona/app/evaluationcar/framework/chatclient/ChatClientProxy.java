d7cb1a2c8c578a83ee00c87c533be5c724de4bf0
moth <moth@gionee.com>
2017-05-06 23:56:25 +0800

    集成环信客服

---
+++ app/src/main/java/com/smona/app/evaluationcar/framework/chatclient/ChatClientProxy.java
@@ -0,0 +1,23 @@
+package com.smona.app.evaluationcar.framework.chatclient;
+
+import android.content.Context;
+
+import com.smona.app.evaluationcar.framework.IProxy;
+
+/**
+ * Created by Moth on 2017/5/6.
+ */
+
+public class ChatClientProxy implements IProxy {
+    public void  init(Context context) {
+        ChatClient.Options options = new ChatClient.Options();
+        options.setAppkey("1112170506115622#kefuchannelapp41042");
+        options.setTenantId("41042");
+        // Kefu SDK 初始化
+        if (!ChatClient.getInstance().init(context, options)){
+            return;
+        }
+        // Kefu EaseUI的初始化
+        UIProvider.getInstance().init(context);
+    }
+}
