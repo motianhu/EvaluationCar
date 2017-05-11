430eb8a3c08034391a7e142bdcb81a57a259b772
moth <moth@gionee.com>
2017-05-11 23:29:15 +0800

    部分机器进入我的资料报错

--- app/src/main/java/com/smona/app/evaluationcar/ui/setting/MineActivity.java
+++ app/src/main/java/com/smona/app/evaluationcar/ui/setting/MineActivity.java
@@ -2,6 +2,7 @@ package com.smona.app.evaluationcar.ui.setting;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -17,6 +18,7 @@ import com.smona.app.evaluationcar.framework.json.JsonParse;
 import com.smona.app.evaluationcar.ui.common.activity.HeaderActivity;
 import com.smona.app.evaluationcar.util.CarLog;
 import com.smona.app.evaluationcar.util.UrlConstants;
+import com.smona.app.evaluationcar.util.ViewUtil;
 
 /**
  * Created by motianhu on 4/14/17.
@@ -25,9 +27,14 @@ import com.smona.app.evaluationcar.util.UrlConstants;
 public class MineActivity extends HeaderActivity {
     private static final String TAG = MineActivity.class.getSimpleName();
 
+    private View mContent;
+    private View mLoading;
+    private View mNoContent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initViews();
         if (mUserBean == null) {
             requestUserInfo();
         } else {
@@ -35,7 +42,20 @@ public class MineActivity extends HeaderActivity {
         }
     }
 
+    public void initViews() {
+        mContent = findViewById(R.id.content);
+        mLoading = findViewById(R.id.loading);
+        mNoContent = findViewById(R.id.no_content);
+        mNoContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestUserInfo();
+            }
+        });
+    }
+
     private void requestUserInfo() {
+        setLoading();
         final UserItem user = new UserItem();
         user.readSelf(this);
         UserParam param = new UserParam();
@@ -46,13 +66,18 @@ public class MineActivity extends HeaderActivity {
             public void onSuccess(String result) {
                 ResUserModel normal = JsonParse.parseJson(result, ResUserModel.class);
                 CarLog.d(TAG, "onSuccess normal: " + normal);
-                String url = UrlConstants.getInterface(UrlConstants.CHECK_USER) + "?userName=" + user.mId;
-                CacheDelegator.getInstance().saveNewCacheByUrl(url, result);
-                refreshViews(normal.object);
+                if(normal.success) {
+                    String url = UrlConstants.getInterface(UrlConstants.CHECK_USER) + "?userName=" + user.mId;
+                    CacheDelegator.getInstance().saveNewCacheByUrl(url, result);
+                    runUI(normal.object);
+                } else {
+                    runUI(null);
+                }
             }
 
             @Override
             public void onFailed(String error) {
+                runUI(null);
                 CarLog.d(TAG, "onError ex: " + error);
             }
         });
@@ -62,13 +87,19 @@ public class MineActivity extends HeaderActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                refreshViews(bean);
+                if(bean != null) {
+                    refreshViews(bean);
+                } else {
+                    setNoData();
+                }
             }
         });
     }
 
 
     private void refreshViews(UserInfoBean bean) {
+        setHasData();
+
         ViewGroup parent = (ViewGroup) findViewById(R.id.name);
         TextView key = (TextView) parent.findViewById(R.id.key);
         key.setText(R.string.mine_display_name);
@@ -81,6 +112,24 @@ public class MineActivity extends HeaderActivity {
         key.setText(R.string.mine_belong_company);
         value = (TextView) parent.findViewById(R.id.value);
         value.setText(bean.companyName);
+    }
+
+    private void setHasData() {
+        ViewUtil.setViewVisible(mNoContent, false);
+        ViewUtil.setViewVisible(mLoading, false);
+        ViewUtil.setViewVisible(mContent, true);
+    }
+
+    private void setNoData() {
+        ViewUtil.setViewVisible(mNoContent, true);
+        ViewUtil.setViewVisible(mLoading, false);
+        ViewUtil.setViewVisible(mContent, false);
+    }
+
+    private void setLoading() {
+        ViewUtil.setViewVisible(mNoContent, false);
+        ViewUtil.setViewVisible(mLoading, true);
+        ViewUtil.setViewVisible(mContent, false);
     }
 
     @Override
